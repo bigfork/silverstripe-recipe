05d7b2a70d30f3cb30095ae30dc9545e3628bb23
Loz Calver <lozcalver@bigfork.co.uk>
2018-03-26 16:08:45 +0100

    Add deployer scripts

---
+++ deploy.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Deployer;
+
+use Symfony\Component\Console\Input\InputOption;
+
+require 'recipe/common.php';
+require 'deploy/config.php';
+require 'deploy/recipe/silverstripe.php';
+
+// Project name
+set('application', function() {
+    if (!defined('DEP_APPLICATION')) {
+        writeln("<error>Please define DEP_APPLICATION in deploy/config.php</error>");
+        exit;
+    }
+
+    return DEP_APPLICATION;
+});
+
+// Project repository
+set('repository', function() {
+    if (!defined('DEP_REPOSITORY')) {
+        writeln("<error>Please define DEP_REPOSITORY in deploy/config.php</error>");
+        exit;
+    }
+
+    return DEP_REPOSITORY;
+});
+
+// Number of releases to keep
+set('keep_releases', 5);
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true);
+
+// Shared files/dirs between deploys
+set('shared_files', [
+    '.env',
+    'debug.log'
+]);
+set('shared_dirs', [
+    'public/assets'
+]);
+
+// Writable dirs by web server
+set('writable_dirs', [
+    'public/assets',
+    'silverstripe-cache'
+]);
+set('allow_anonymous_stats', false);
+
+// Hosts
+set('default_stage', 'prod');
+
+host('stickyfork')
+    ->stage('prod')
+    ->hostname('stickyfork')
+    ->roles('app')
+    ->set('deploy_path', function() {
+        if (defined('DEP_DEPLOY_PATH')) {
+            return DEP_DEPLOY_PATH;
+        }
+        return '/var/www/vhosts/live/{{application}}';
+    });
+
+host('stickyfork-stage')
+    ->stage('stage')
+    ->hostname('stickyfork')
+    ->roles('app')
+    ->set('deploy_path', function() {
+        if (defined('DEP_DEPLOY_STAGE_PATH')) {
+            return DEP_DEPLOY_STAGE_PATH;
+        }
+        return '/var/www/vhosts/stage/{{application}}';
+    });
+
+desc('Deploy your project');
+option('include-assets', null, InputOption::VALUE_NONE, 'Overwrite assets from local assets directory');
+option('include-db', null, InputOption::VALUE_NONE, 'Overwrite database from local database');
+task('deploy', function () {
+    invoke('deploy:info');
+    invoke('deploy:prepare');
+    invoke('deploy:lock');
+    invoke('deploy:release');
+    invoke('deploy:update_code');
+    invoke('silverstripe:create_dotenv');
+    invoke('silverstripe:create_cache_dir');
+    invoke('deploy:shared');
+    invoke('deploy:writable');
+    invoke('deploy:vendors');
+    invoke('silverstripe:vendor_expose');
+    if (input()->getOption('include-assets')) {
+        invoke('silverstripe:upload_assets');
+    }
+    if (input()->getOption('include-db')) {
+        invoke('silverstripe:upload_database');
+    }
+    invoke('silverstripe:dev_build');
+    invoke('deploy:clear_paths');
+    invoke('deploy:symlink');
+    invoke('deploy:unlock');
+    invoke('cleanup');
+    invoke('success');
+});
+
+// [Optional] If deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
---
+++ deploy/recipe/silverstripe.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace Deployer;
+
+use Deployer\Task\Context;
+use RuntimeException;
+
+// Tasks
+desc('Populate .env file');
+task('silverstripe:create_dotenv', function () {
+    $envPath = "{{deploy_path}}/shared/.env";
+    if (test("[ -f {$envPath} ]")) {
+        return;
+    }
+
+    $dbServer = ask('Please enter the database server', 'localhost');
+    $dbUser = ask('Please enter the database username');
+    $dbPass = str_replace("'", "\\'", askHiddenResponse('Please enter the database password'));
+    $dbName = ask('Please enter the database name', get('application'));
+    $type = Context::get()->getHost()->getConfig()->get('stage') === 'stage' ? 'test' : 'live';
+
+    $contents = <<<ENV
+SS_DATABASE_CLASS='MySQLPDODatabase'
+SS_DATABASE_USERNAME='{$dbUser}'
+SS_DATABASE_PASSWORD='{$dbPass}'
+SS_DATABASE_SERVER='{$dbServer}'
+SS_DATABASE_NAME='{$dbName}'
+SS_ENVIRONMENT_TYPE='{$type}'
+ENV;
+
+    $command = <<<BASH
+cat >{$envPath} <<EOL
+$contents
+EOL
+BASH;
+
+    run("$command");
+})->setPrivate();
+
+desc('Run composer vendor-expose');
+task('silverstripe:vendor_expose', function () {
+    run('cd {{release_path}} && {{bin/composer}} vendor-expose');
+});
+
+desc('Create silverstripe-cache directory');
+task('silverstripe:create_cache_dir', function () {
+    run("cd {{release_path}} && if [ ! -d silverstripe-cache ]; then mkdir silverstripe-cache; fi");
+})->setPrivate();
+
+desc('Run dev/build');
+task('silverstripe:dev_build', function () {
+    $httpUser = get('http_user', false);
+    if ($httpUser === false) {
+        // Detect http user in process list.
+        $httpUser = run("ps axo user,comm | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d ' ' -f1");
+        if (empty($httpUser)) {
+            throw new RuntimeException(
+                "Can't detect http user name.\n" .
+                "Please setup `http_user` config parameter."
+            );
+        }
+    }
+
+    run("sudo -u {$httpUser} {{release_path}}/vendor/bin/sake dev/build flush");
+});
+
+desc('Create directory for sspak dumps');
+task('silverstripe:create_dump_dir', function () {
+    run("cd {{deploy_path}} && if [ ! -d dumps ]; then mkdir dumps; fi");
+})->setPrivate();
+
+desc('Upload assets');
+task('silverstripe:upload_assets', function () {
+    $filename = get('application') . '-assets-' . date('Y-m-d-H-i-s') . '.sspak';
+    $local = sys_get_temp_dir() . '/' . $filename;
+
+    // Dump assets from local copy and upload
+    runLocally("sspak save --assets . $local");
+    upload($local, "{{deploy_path}}/dumps/");
+
+    // Deploy assets
+    run("cd {{release_path}} && sspak load --assets {{deploy_path}}/dumps/{$filename}");
+
+    // Tidy up
+    runLocally("rm $local");
+    run("rm {{deploy_path}}/dumps/{$filename}");
+});
+before('silverstripe:upload_assets', 'silverstripe:create_dump_dir');
+after('silverstripe:upload_assets', 'deploy:writable');
+
+desc('Upload database');
+task('silverstripe:upload_database', function () {
+    $filename = get('application') . '-db-' . date('Y-m-d-H-i-s') . '.sspak';
+    $local = sys_get_temp_dir() . '/' . $filename;
+
+    // Dump database from local copy and upload
+    runLocally("sspak save --db . $local");
+    upload($local, "{{deploy_path}}/dumps/");
+
+    // Deploy database
+    run("cd {{release_path}} && sspak load --db {{deploy_path}}/dumps/{$filename}");
+
+    // Tidy up
+    runLocally("rm $local");
+    run("rm {{deploy_path}}/dumps/{$filename}");
+});
+before('silverstripe:upload_database', 'silverstripe:create_dump_dir');
+
+desc('Download assets');
+task('silverstripe:download_assets', function () {
+    $filename = get('application') . '-assets-' . date('Y-m-d-H-i-s') . '.sspak';
+    $local = sys_get_temp_dir() . '/' . $filename;
+
+    // Dump assets from remote copy and download
+    run("cd {{release_path}} && sspak save --assets . {{deploy_path}}/dumps/{$filename}");
+    download("{{deploy_path}}/dumps/{$filename}", $local);
+
+    // Import assets
+    runLocally("sspak load --assets {$local}");
+
+    // Tidy up
+    runLocally("rm $local");
+    run("rm {{deploy_path}}/dumps/{$filename}");
+});
+before('silverstripe:download_assets', 'silverstripe:create_dump_dir');
+
+desc('Download database');
+task('silverstripe:download_database', function () {
+    $filename = get('application') . '-db-' . date('Y-m-d-H-i-s') . '.sspak';
+    $local = sys_get_temp_dir() . '/' . $filename;
+
+    // Dump database from remote copy and download
+    run("cd {{release_path}} && sspak save --db . {{deploy_path}}/dumps/{$filename}");
+    download("{{deploy_path}}/dumps/{$filename}", $local);
+
+    // Import database
+    runLocally("sspak load --db {$local}");
+
+    // Tidy up
+    runLocally("rm $local");
+    run("rm {{deploy_path}}/dumps/{$filename}");
+});
+before('silverstripe:download_database', 'silverstripe:create_dump_dir');
