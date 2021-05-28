df1066b17fd5de3920ab99c1836d8ab8a0eb5435
Kris <kriscoidan@bigfork.co.uk>
2021-05-28 14:05:24 +0100

    Added hr to TinyMCE config and default styles.

--- app/_config.php
+++ app/_config.php
@@ -9,9 +9,9 @@ i18n::set_locale('en_GB');
 // TinyMCE Config
 $config = TinyMCEConfig::get('cms');
 $config->disablePlugins(['importcss', 'contextmenu']);
-$config->enablePlugins(['anchor']);
+$config->enablePlugins(['anchor', 'hr']);
 $config->setButtonsForLine(1, 'formatselect styleselect | bullist numlist | bold italic subscript superscript |
-    sslink unlink anchor ssmedia ssembed');
+    sslink unlink anchor ssmedia ssembed hr');
 $config->setButtonsForLine(2, 'table | pastetext undo redo | code');
 $config->setOptions([
     'block_formats' => 'Paragraph=p;Heading 2=h2;Heading 3=h3',
