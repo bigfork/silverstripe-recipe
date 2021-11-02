95c875f40cec306b4bdc7466953126b96969238b
Loz Calver <lozcalver@bigfork.co.uk>
2021-11-02 15:23:05 +0000

    Deployer updates

--- deploy.php
+++ deploy.php
@@ -5,28 +5,7 @@ namespace Deployer;
 use Symfony\Component\Console\Input\InputOption;
 
 require 'recipe/common.php';
-require 'deploy/config.php';
 require 'deploy/recipe/silverstripe.php';
-
-// Project name
-set('application', function() {
-    if (!defined('DEP_APPLICATION')) {
-        writeln("<error>Please define DEP_APPLICATION in deploy/config.php</error>");
-        exit;
-    }
-
-    return DEP_APPLICATION;
-});
-
-// Project repository
-set('repository', function() {
-    if (!defined('DEP_REPOSITORY')) {
-        writeln("<error>Please define DEP_REPOSITORY in deploy/config.php</error>");
-        exit;
-    }
-
-    return DEP_REPOSITORY;
-});
 
 // Number of releases to keep
 set('keep_releases', 5);
@@ -53,37 +32,10 @@ set('allow_anonymous_stats', false);
 
 // Hosts
 set('default_stage', 'staging');
-
-// Production aliases
-foreach (['production', 'prod', 'live'] as $alias) {
-    host($alias)
-        ->stage($alias)
-        ->hostname('stickyfork')
-        ->roles('app')
-        ->set('deploy_path', function () {
-            if (defined('DEP_DEPLOY_PATH')) {
-                return DEP_DEPLOY_PATH;
-            }
-            return '/var/www/vhosts/live/{{application}}';
-        });
-}
-
-// Staging aliases
-foreach (['staging', 'stage', 'test'] as $alias) {
-    host($alias)
-        ->stage($alias)
-        ->hostname('stickyfork')
-        ->roles('app')
-        ->set('deploy_path', function () {
-            if (defined('DEP_DEPLOY_STAGE_PATH')) {
-                return DEP_DEPLOY_STAGE_PATH;
-            }
-            return '/var/www/vhosts/stage/{{application}}';
-        });
-}
+inventory('deploy/hosts.yml');
 
 desc('Deploy your project');
-task('deploy', function () {
+task('deploy', function() {
     invoke('deploy:info');
     invoke('deploy:prepare');
     invoke('deploy:lock');
--- deploy/recipe/silverstripe.php
+++ deploy/recipe/silverstripe.php
@@ -16,11 +16,11 @@ task('silverstripe:create_dotenv', function () {
     $dbServer = ask('Please enter the database server', 'localhost');
     $dbUser = ask('Please enter the database username');
     $dbPass = str_replace("'", "\\'", askHiddenResponse('Please enter the database password'));
-    $dbName = ask('Please enter the database name', get('application'));
+    $dbName = ask('Please enter the database name');
     $sentryDSN = ask('Please enter the Sentry DSN (if applicable)');
     $stage = Context::get()->getHost()->getConfig()->get('stage');
-    $dbPrefix = in_array($stage, ['test', 'stage', 'staging']) ? '_stage_' : '';
-    $type = in_array($stage, ['test', 'stage', 'staging']) ? 'test' : 'live';
+    $dbPrefix = $stage === 'production' ? '' : '_stage_';
+    $type = $stage === 'production' ? 'live' : 'test';
 
     $contents = <<<ENV
 SS_DATABASE_CLASS='MySQLDatabase'
@@ -57,19 +57,7 @@ task('silverstripe:create_cache_dir', function () {
 
 desc('Run dev/build');
 task('silverstripe:dev_build', function () {
-    $httpUser = get('http_user', false);
-    if ($httpUser === false) {
-        // Detect http user in process list.
-        $httpUser = run("ps axo user,comm | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d ' ' -f1");
-        if (empty($httpUser)) {
-            throw new RuntimeException(
-                "Can't detect http user name.\n" .
-                "Please setup `http_user` config parameter."
-            );
-        }
-    }
-
-    run("sudo -u {$httpUser} {{release_path}}/vendor/bin/sake dev/build flush");
+    run("{{release_path}}/vendor/bin/sake dev/build flush");
 });
 
 desc('Create directory for sspak dumps');
@@ -79,81 +67,35 @@ task('silverstripe:create_dump_dir', function () {
 
 desc('Upload assets');
 task('silverstripe:upload_assets', function () {
-    $filename = get('application') . '-assets-' . date('Y-m-d-H-i-s') . '.sspak';
-    $local = sys_get_temp_dir() . '/' . $filename;
-
-    // Dump assets from local copy and upload
-    runLocally("sspak save --assets . $local", [
-        'timeout' => 1800
+    upload('public/assets/', '{{deploy_path}}/shared/public/assets', [
+        'options' => [
+            "--exclude={'error-*.html','.htaccess','.DS_Store'}",
+            "--delete"
+        ]
     ]);
-    upload($local, "{{deploy_path}}/dumps/");
-
-    // Deploy assets
-    run("cd {{release_path}} && sspak load --assets {{deploy_path}}/dumps/{$filename}", [
-        'timeout' => 1800
-    ]);
-
-    // Tidy up
-    runLocally("rm $local");
-    run("rm {{deploy_path}}/dumps/{$filename}");
 });
-before('silverstripe:upload_assets', 'silverstripe:create_dump_dir');
 after('silverstripe:upload_assets', 'deploy:writable');
 
 desc('Upload database');
 task('silverstripe:upload_database', function () {
-    $filename = get('application') . '-db-' . date('Y-m-d-H-i-s') . '.sspak';
-    $local = sys_get_temp_dir() . '/' . $filename;
-
-    // Dump database from local copy and upload
-    runLocally("sspak save --db . $local");
-    upload($local, "{{deploy_path}}/dumps/");
-
-    // Deploy database
-    run("cd {{release_path}} && sspak load --db {{deploy_path}}/dumps/{$filename}");
-
-    // Tidy up
-    runLocally("rm $local");
-    run("rm {{deploy_path}}/dumps/{$filename}");
+    writeln("<error>This task is currently disabled.</error>");
+    exit;
 });
 before('silverstripe:upload_database', 'silverstripe:create_dump_dir');
 
 desc('Download assets');
 task('silverstripe:download_assets', function () {
-    $filename = get('application') . '-assets-' . date('Y-m-d-H-i-s') . '.sspak';
-    $local = sys_get_temp_dir() . '/' . $filename;
-
-    // Dump assets from remote copy and download
-    run("cd {{release_path}} && sspak save --assets . {{deploy_path}}/dumps/{$filename}", [
-        'timeout' => 1800
+    download('{{deploy_path}}/shared/public/assets/', 'public/assets', [
+        'options' => [
+            "--exclude={'error-*.html','.htaccess'}",
+            "--delete"
+        ]
     ]);
-    download("{{deploy_path}}/dumps/{$filename}", $local);
-
-    // Import assets
-    runLocally("sspak load --assets {$local}", [
-        'timeout' => 1800
-    ]);
-
-    // Tidy up
-    runLocally("rm $local");
-    run("rm {{deploy_path}}/dumps/{$filename}");
 });
-before('silverstripe:download_assets', 'silverstripe:create_dump_dir');
 
 desc('Download database');
 task('silverstripe:download_database', function () {
-    $filename = get('application') . '-db-' . date('Y-m-d-H-i-s') . '.sspak';
-    $local = sys_get_temp_dir() . '/' . $filename;
-
-    // Dump database from remote copy and download
-    run("cd {{release_path}} && sspak save --db . {{deploy_path}}/dumps/{$filename}");
-    download("{{deploy_path}}/dumps/{$filename}", $local);
-
-    // Import database
-    runLocally("sspak load --db {$local}");
-
-    // Tidy up
-    runLocally("rm $local");
-    run("rm {{deploy_path}}/dumps/{$filename}");
+    writeln("<error>This task is currently disabled.</error>");
+    exit;
 });
 before('silverstripe:download_database', 'silverstripe:create_dump_dir');
