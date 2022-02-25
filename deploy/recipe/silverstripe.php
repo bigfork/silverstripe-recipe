a00c2454ad8a17670d571e07e3338d3909068c4b
Loz Calver <lozcalver@bigfork.co.uk>
2022-02-25 15:52:41 +0000

    Update database tasks to be platform-agnostic

--- deploy/recipe/silverstripe.php
+++ deploy/recipe/silverstripe.php
@@ -83,8 +83,29 @@ after('silverstripe:upload_assets', 'deploy:writable');
 
 desc('Upload database');
 task('silverstripe:upload_database', function () {
-    writeln("<error>This task is currently disabled.</error>");
-    exit;
+    if (!testLocally('[ -f .env ]')) {
+        writeln("<error>Unable to find .env file in local environment.</error>");
+        exit;
+    } elseif (!test('[ -f {{deploy_path}}/shared/.env ]')) {
+        writeln("<error>Unable to find .env file on remote server.</error>");
+        exit;
+    }
+
+    $filename = 'db-' . date('Y-m-d-H-i-s') . '.gz';
+    $localPath = sys_get_temp_dir() . '/' . $filename;
+
+    // Export database
+    runLocally(getExportDatabaseCommand('.env', $localPath));
+
+    // Upload database
+    upload($localPath, "{{deploy_path}}/dumps/");
+
+    // Import database
+    run(getImportDatabaseCommand('{{deploy_path}}/shared/.env', "{{deploy_path}}/dumps/{$filename}"));
+
+    // Tidy up
+    runLocally("rm {$localPath}");
+    run("rm {{deploy_path}}/dumps/{$filename}");
 });
 before('silverstripe:upload_database', 'silverstripe:create_dump_dir');
 
@@ -100,7 +121,78 @@ task('silverstripe:download_assets', function () {
 
 desc('Download database');
 task('silverstripe:download_database', function () {
-    writeln("<error>This task is currently disabled.</error>");
-    exit;
+    if (!testLocally('[ -f .env ]')) {
+        writeln("<error>Unable to find .env file in local environment.</error>");
+        exit;
+    } elseif (!test('[ -f {{deploy_path}}/shared/.env ]')) {
+        writeln("<error>Unable to find .env file on remote server.</error>");
+        exit;
+    }
+
+    $filename = 'db-' . date('Y-m-d-H-i-s') . '.gz';
+    $localPath = sys_get_temp_dir() . '/' . $filename;
+
+    // Export database
+    run(getExportDatabaseCommand('{{deploy_path}}/shared/.env', "{{deploy_path}}/dumps/{$filename}"));
+
+    // Download database
+    download("{{deploy_path}}/dumps/{$filename}", $localPath);
+
+    // Import database
+    runLocally(getImportDatabaseCommand('.env', $localPath));
+
+    // Tidy up
+    runLocally("rm {$localPath}");
+    run("rm {{deploy_path}}/dumps/{$filename}");
 });
 before('silverstripe:download_database', 'silverstripe:create_dump_dir');
+
+set('mysql_default_charset', 'utf8');
+set(
+    'mysqldump_args',
+    implode(' ', [
+        '--no-tablespaces',
+        '--skip-opt',
+        '--add-drop-table',
+        '--extended-insert',
+        '--create-options',
+        '--quick',
+        '--set-charset',
+        '--default-character-set={{mysql_default_charset}}'
+    ])
+);
+
+set(
+    'mysql_args',
+    implode(' ', [
+        '--default-character-set={{mysql_default_charset}}'
+    ])
+);
+
+function getExportDatabaseCommand($envPath, $destination) {
+    $usernameArg = '--user=$SS_DATABASE_USERNAME';
+    $passwordArg = '--password=$SS_DATABASE_PASSWORD';
+    $hostArg = '--host=$SS_DATABASE_SERVER';
+    $databaseArg = '$SS_DATABASE_PREFIX$SS_DATABASE_NAME';
+
+    $loadEnvCmd = "export $(grep -v '^#' {$envPath} | xargs)";
+
+    $exportDbCmd = "mysqldump {{mysqldump_args}} {$usernameArg} {$passwordArg} {$hostArg} {$databaseArg} | gzip > {$destination}";
+    return "{$loadEnvCmd} && {$exportDbCmd}";
+}
+
+function getImportDatabaseCommand($envPath, $source) {
+    $usernameArg = '--user=$SS_DATABASE_USERNAME';
+    $passwordArg = '--password=$SS_DATABASE_PASSWORD';
+    $hostArg = '--host=$SS_DATABASE_SERVER';
+    $databaseArg = '$SS_DATABASE_PREFIX$SS_DATABASE_NAME';
+
+    $loadEnvCmd = "export $(grep -v '^#' {$envPath} | xargs)";
+
+    $createDbArg = "--execute='create database if not exists `'{$databaseArg}'`;'";
+    $createDbCmd = "mysql {{mysql_args}} {$usernameArg} {$passwordArg} {$hostArg} {$createDbArg}";
+
+    $importDbCmd = "gunzip < {$source} | mysql {{mysql_args}} {$usernameArg} {$passwordArg} {$hostArg} {$databaseArg}";
+
+    return "{$loadEnvCmd} && {$createDbCmd} && {$importDbCmd}";
+}
