34503d29192d81aa202be5069fcfc4b0d055156e
Loz Calver <kinglozzer@gmail.com>
2022-06-29 14:23:14 +0100

    Stop duplicate "Page" showing in CMS

--- app/src/bootstrap.php
+++ app/src/bootstrap.php
@@ -6,6 +6,15 @@ use SilverStripe\CMS\Model\SiteTree;
 class Page extends SiteTree
 {
     private static $table_name = 'SilverStripe_Page';
+
+    public function canCreate($member = null, $context = [])
+    {
+        if (static::class === \App\Model\Page::class) {
+            return false;
+        }
+
+        return parent::canCreate($member, $context);
+    }
 }
 
 class PageController extends ContentController
