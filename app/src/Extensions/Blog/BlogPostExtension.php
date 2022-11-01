be3fb285cfd5d8f87c233ffb07bf0820adf38793
Kris <kriscoidan@bigfork.co.uk>
2022-11-01 10:34:37 +0000

    Removes doc comment in BlogExtension and removes the setting of Extra Class on the summary field as this is done by setting a default class in forms.yml

--- app/src/Extensions/Blog/BlogPostExtension.php
+++ app/src/Extensions/Blog/BlogPostExtension.php
@@ -5,20 +5,11 @@ namespace App\Extensions\Blog;
 use SilverStripe\Core\Extension;
 use SilverStripe\Forms\FieldList;
 
-/**
- * Class BlogPostExtension
- * @package App\Extensions\Blog
- */
 class BlogPostExtension extends Extension
 {
-    /**
-     * @param FieldList $fields
-     * @return void
-     */
     public function updateCMSFields(FieldList $fields): void
     {
         $summaryHTMLEditor = $fields->dataFieldByName('Summary');
-        $summaryHTMLEditor->addExtraClass('stacked');
         $fields->insertBefore('CustomSummary', $summaryHTMLEditor);
         $fields->removeByName('CustomSummary');
     }
