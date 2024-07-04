b2538c31b6f8f85b9e7e0e634901fe3e568d52c2
Loz Calver <kinglozzer@gmail.com>
2024-07-04 16:55:38 +0100

    Add translations to make linkfield easier to understand

---
+++ app/lang/en.js
@@ -0,0 +1,8 @@
+if (typeof(ss) === 'undefined' || typeof(ss.i18n) === 'undefined') {
+  console.error('Class ss.i18n not defined');
+} else {
+  ss.i18n.addDictionary('en', {
+    "LinkField.ARCHIVE": "Clear",
+    "LinkField.DELETE": "Clear"
+  });
+}
