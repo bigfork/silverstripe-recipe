10d7ac892a4305dc2a9e9c83d50864035fc39f43
Loz Calver <kinglozzer@gmail.com>
2025-03-18 12:49:41 +0000

    Don't allow creating blog posts outside of blogs

--- app/src/Extensions/Blog/BlogPostExtension.php
+++ app/src/Extensions/Blog/BlogPostExtension.php
@@ -3,11 +3,22 @@
 namespace App\Extensions\Blog;
 
 use SilverStripe\AssetAdmin\Forms\UploadField;
+use SilverStripe\Blog\Model\Blog;
 use SilverStripe\Core\Extension;
 use SilverStripe\Forms\FieldList;
 
 class BlogPostExtension extends Extension
 {
+    public function canCreate($member = null, $context = []): ?bool
+    {
+        $parent = $context['Parent'] ?? null;
+        if (!$parent instanceof Blog) {
+            return false;
+        }
+
+        return null;
+    }
+
     public function updateCMSFields(FieldList $fields): void
     {
         // Move summary out of ToggleCompositeField
